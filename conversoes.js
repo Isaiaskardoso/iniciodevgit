0f8d93466670215fa586a9a85da83e590c159270
A. Isaias Kardoso <isaiaskardososilva@gmail.com>
2022-05-17 18:05:19 -0300

    variaveis,  conversoes e boolean

---
+++ conversoes.js
@@ -0,0 +1,17 @@
+// tipo de dado
+// juntos com booleanos
+
+// conversao implicida
+// numero é diferente de string
+
+const numero = 456;
+const numeroString = "456";
+//console.log(numero + numeroString)
+
+// conversao explicita
+// Number()
+// String()
+console.log(numero + Number(numeroString))
+
+
+
--- seraBoolean.js
+++ seraBoolean.js
@@ -9,3 +9,20 @@ const texto1 = "Mais é JS";
 const texto2 = "js";
 
 console.log(texto1 === texto2)
+
+// algumas companias - B.C - usa os tipy trufh ou falsy
+// 0 => false
+// 1 = true
+
+// console.log(0 == false)
+// console.log("" == false)
+// console.log(1 == true)
+
+// undefined
+// null ==> vazio ou nada
+
+let minhaVar;
+let varNull = null;
+
+console.log(minhaVar)
+console.log(varNull)
